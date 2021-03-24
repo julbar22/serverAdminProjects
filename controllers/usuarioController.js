1c20b3b34225deb55bdf11eb4aa347ad8ee931d3
julbar22 <julian318barbosa@gmail.com>
2021-03-23 23:58:27 -0300

    Se añade bcrypt para las contraseñas y express validator para los request

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,3 +1,27 @@
-exports.crearUsuario = (req, res) => {
-    const usuario = req.body;
+const Usuario = require('../models/Usuario');
+const bcryptjs = require('bcryptjs');
+const { validationResult } = require('express-validator')
+
+exports.crearUsuario = async (req, res) => {
+
+    const errores = validationResult(req);
+    if (!errores.isEmpty()) {
+        res.status(400).json({ errores: errores.array() })
+    }
+
+    const { email, password } = req.body;
+    try {
+        let usuario = await Usuario.findOne({ email })
+        if (usuario) {
+            res.status(400).json({ msg: 'El usuario ya existe' });
+        }
+        usuario = Usuario(req.body);
+        const salt = await bcryptjs.genSalt(10);
+        usuario.password = await bcryptjs.hash(password, salt);
+        await usuario.save();
+        res.status(200).json({ msg: 'Usuario creado correctamente' });
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({ msg: 'Error al crear el usuario' });
+    }
 }
--- routes/usuario.js
+++ routes/usuario.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuarioController');
+const { check } = require('express-validator');
 // baseurl = api/usuarios
 router.post('/',
+    [
+        check('nombre', 'El nombre es obligatorio').notEmpty(),
+        check('email', 'El email no es valido').isEmail(),
+        check('password', 'El password debe ser minimo de 6 caracteres').isLength({ min: 6 })
+    ],
     usuarioController.crearUsuario
 );
 
